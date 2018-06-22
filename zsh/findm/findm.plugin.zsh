b1315d6434e24ecf1e03a7d32bf692a5bef30244
Carlos Gajardo <cjgajard@gmail.com>
2018-06-22 05:04:36 -0400

    Include zsh plugins

---
+++ zsh/findm/findm.plugin.zsh
@@ -0,0 +1,44 @@
+# find files by name using find -name
+# Example:
+#   findm --exclude=\*.{sh,py,js,css} -Ir "test" *
+
+function findm() {
+    query=false
+    exclude=()
+    look_for_extension=false
+
+    # go through all parametres, to separate parameters from the query
+    # string
+    for s in $*
+    do
+        case $s in 
+            --django)
+                exclude+="-not -path \"./.env/*\""
+                exclude+='-not -path "./node_modules/*"'
+                exclude+='-not -path "./base/static/bower_components/*"'
+                exclude+='-not -path "./fixtures/*"'
+                exclude+='-not -path "./CACHE/*"'
+                ;;
+            -e)
+                look_for_extension=true
+                ;;
+            -*)
+                # this is a parameter for grepr
+                params+=$s
+                ;;
+            *) 
+                if [ $query = false ] ; then
+                    query="$s"
+                else
+                    query="$query $s"
+                fi
+                ;; 
+        esac
+    done
+
+    if [ $look_for_extension=false ] ; then
+        eval "find .  $params -name \"$query*\" $exclude"
+    else
+        eval "find .  $params -name \"*.$query\" $exclude"
+    fi
+}
---
+++ zsh/grepr/grepr.plugin.zsh
@@ -0,0 +1,61 @@
+# grep recursive through all
+# Example:
+#   grep --exclude=\*.{sh,py,js,css} -Ir "test" *
+
+function grepr(){
+    fuzzy=false
+    query=false
+    params=()
+
+    # go through all parametres, to separate parameters from the query
+    # string
+    for s in $*
+    do
+        case $s in 
+            --django)
+                params+="--exclude-dir=\.env"
+                params+="--exclude-dir=node_modules"
+                params+="--exclude-dir=bower_components"
+                params+="--exclude-dir=fixtures"
+                params+="--exclude-dir=CACHE"
+                params+="--exclude=*.sql"
+                params+="--exclude=*.log"
+                ;;
+            --js)
+                params+="--exclude-dir=node_modules"
+                params+="--exclude-dir=dist"
+                params+="--exclude=package-lock.json"
+                ;;
+            --em)
+                params+="--exclude-dir=migrations"
+                ;;
+            --es)
+                params+="--exclude-dir=static"
+                ;;
+            --ev)
+                params+="--exclude-dir=vendor"
+                ;;
+            --fu)
+                fuzzy=true
+                ;;
+            -*)
+                # this is a parameter for grepr
+                params+=$s
+                ;;
+            *) 
+                if [ $query = false ] ; then
+                    query="$s"
+                else
+                    query="$query $s"
+                fi
+                ;; 
+        esac
+    done
+
+    if [ $fuzzy != false ] ; then
+        query=$(echo $query | sed 's/\( \)/.*/g')
+        echo $query
+    fi
+
+    grep $params -Inr "$query" *
+}
