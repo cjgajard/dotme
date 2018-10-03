8a60332bb1d7680a0458e25d722b17c543300dbc
Carlos Gajardo <cjgajard@gmail.com>
2018-10-03 16:45:52 -0300

    Refactor install.sh script

--- install.sh
+++ install.sh
@@ -1,33 +1,39 @@
 if [ ! $CONFIGFOLDER ]; then
-    export CONFIGFOLDER=$(readlink -f .)
+    export CONFIGFOLDER="$(cd `dirname ${BASH_SOURCE}`; pwd -P)"
 fi
 
 if [ ! $ZSH_CUSTOM ]; then
     export ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 fi
 
-clean() {
-    rm $HOME/.vimrc
-    rm $HOME/.zshrc
-    rm $HOME/.tmux.conf
-    rm $ZSH_CUSTOM/aliases.sh
-    true
+configfolder_install() {
+    local usage="usage: configfolder_install SOURCE_FILE DESTINATION_FILE"
+    [[ "$1" == "" || "$2" == "" ]] && echo $usage && return 1
+    [[ ! -f $1 ]] && echo "file \`$1\` does not exist" && return 1
+    [[ -f $2 ]] && rm $2
+    ln -s $1 $2
 }
 
-install_vim() {
-    ln -s $CONFIGFOLDER/vim/vimrc $HOME/.vimrc
+configfolder_vim() {
+    configfolder_install $CONFIGFOLDER/vim/vimrc $HOME/.vimrc
 }
 
-install_zsh() {
-    ln -s $CONFIGFOLDER/zsh/zshrc $HOME/.zshrc
-    ln -s $CONFIGFOLDER/zsh/aliases.sh $ZSH_CUSTOM/aliases.sh
+configfolder_bash() {
+    configfolder_install $CONFIGFOLDER/bash/bashrc $HOME/.bashrc
 }
 
-install_tmux() {
-    ln -s $CONFIGFOLDER/tmux/tmux.conf $HOME/.tmux.conf
+configfolder_zsh() {
+    configfolder_install $CONFIGFOLDER/zsh/zshrc $HOME/.zshrc
+    configfolder_install $CONFIGFOLDER/zsh/aliases.sh $ZSH_CUSTOM/aliases
 }
 
-clean
-install_vim
-install_zsh
-install_tmux
+configfolder_tmux() {
+    configfolder_install $CONFIGFOLDER/tmux/tmux.conf $HOME/.tmux.conf
+}
+
+if [ $0 == "$BASH_SOURCE" ]; then
+    configfolder_vim
+    configfolder_bash
+    configfolder_zsh
+    configfolder_tmux
+fi
--- vim/ftplugin/c.vim
+++ vim/ftplugin/c.vim
@@ -1,2 +1,2 @@
-setlocal shiftwidth=8 tabstop=8 sts=8 noexpandtab
+setlocal shiftwidth=8 tabstop=8 softtabstop=8 noexpandtab
 nnoremap <buffer> <F5> :execute "echo system('make run')"<CR>
