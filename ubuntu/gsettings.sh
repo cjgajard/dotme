af7bb625412e75ba5ba6f3d71636f15149e3ad3f
Carlos Gajardo <cjgajard@gmail.com>
2019-03-27 19:46:24 -0300

    Add gsettings

--- ubuntu/gsettings.sh
+++ ubuntu/gsettings.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.nautilus.desktop trash-icon-visible false
+gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
