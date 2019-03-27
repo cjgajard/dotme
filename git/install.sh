28281509c9060d830594a346ff24bfcf43952a6f
Carlos Gajardo <cjgajard@gmail.com>
2019-03-27 19:54:20 -0300

    Add git install script

---
+++ git/install.sh
@@ -0,0 +1,8 @@
+git config --global user.name 'Carlos Gajardo'
+echo -n 'Insert your email: '; read email
+git config --global user.email $email
+git config --global alias.ap 'add --patch'
+git config --global alias.co 'checkout'
+git config --global alias.cof 'checkout --'
+git config --global alias.logx 'log --graph --oneline'
+git config --global alias.st 'status'
