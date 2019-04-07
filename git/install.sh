abd128634212b7c38a93df8fec7f29df29a3a3b6
Carlos Gajardo <cjgajard@gmail.com>
2019-04-07 17:51:49 -0400

    Add checkout branch to git install script

--- git/install.sh
+++ git/install.sh
@@ -3,6 +3,7 @@ echo -n 'Insert your email: '; read email
 git config --global user.email $email
 git config --global alias.ap 'add --patch'
 git config --global alias.co 'checkout'
+git config --global alias.cob 'checkout -b'
 git config --global alias.cof 'checkout --'
 git config --global alias.logx 'log --graph --oneline'
 git config --global alias.st 'status'
