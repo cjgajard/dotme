edc1041d1ca3d198cd894af5072f915b7c0164cd
Carlos Gajardo <cjgajard@gmail.com>
2019-08-27 04:30:19 -0400

    Update bash 'user' plugin

--- bash/plugin/user.sh
+++ bash/plugin/user.sh
@@ -4,7 +4,7 @@ alias rnra="react-native run-android"
 alias rnri="react-native run-ios"
 REACTNATIVE
 
-#: <<'MISC'
 alias prettyjson='python3 -m json.tool'
 alias tree="tree -a -I '.git|node_modules|.env|__pycache__'"
-#export PATH="$(systemd-path user-binaries):$PATH"
+alias killdiscord="ps -ax | grep discord | grep -v grep | awk '{print \$1}' | xargs kill -9"
+export PATH="$(systemd-path user-binaries):$PATH"
