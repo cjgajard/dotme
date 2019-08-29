a5d25562ebb9c88dd88df8a40e8495d8e5214d34
Carlos Gajardo <cjgajard@gmail.com>
2019-08-29 17:28:19 -0400

    Add git editor

--- git/install.sh
+++ git/install.sh
@@ -1,9 +1,16 @@
 . `dirname ${BASH_SOURCE}`/../bash/lib/configfolder.sh
 
 configfolder_git() {
+  configfolder_install git/gitignore ~/.gitignore
+  git config --global core.excludesfile ~/.gitignore
+  git config --global core.editor vim
+
   git config --global user.name 'Carlos Gajardo'
   echo -n 'Insert your email: '; read email
-  git config --global user.email $email
+  if [ -n "$email" ]; then
+    git config --global user.email $email
+  fi
+
   git config --global alias.ap 'add --patch'
   git config --global alias.co 'checkout'
   git config --global alias.cob 'checkout -b'
@@ -12,8 +19,6 @@ configfolder_git() {
   git config --global alias.lc '!git log --oneline | head -n1'
   git config --global alias.logx 'log --graph --oneline'
   git config --global alias.st 'status'
-  configfolder_install git/gitignore ~/.gitignore
-  git config --global core.excludesfile ~/.gitignore
 }
 
 if [ $0 == "$BASH_SOURCE" ]; then
