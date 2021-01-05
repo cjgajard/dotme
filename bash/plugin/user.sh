e24f698e4842397762849e184f84c060fe780794
Carlos Gajardo <cjgajard@gmail.com>
2021-01-05 02:58:44 -0300

    Make killdiscord a generic function

--- bash/plugin/user.sh
+++ bash/plugin/user.sh
@@ -1,10 +1,10 @@
-: <<'REACTNATIVE'
-alias rn="react-native"
-alias rnra="react-native run-android"
-alias rnri="react-native run-ios"
-REACTNATIVE
-
+fnkill () {
+  if [ -z "$1" ]; then
+    echo "Usage: ${FUNCNAME[0]} [grep-options] SEARCH_TEXT"
+    return 1
+  fi
+  whoami | xargs ps -o pid,cmd,args -u | grep "$@" | grep -v grep \
+    | awk '{print $1}' | xargs kill -9
+}
 alias prettyjson='python3 -m json.tool'
 alias tree="tree -a -I '.git|node_modules|.env|__pycache__'"
-alias killdiscord="ps -ax | grep discord | grep -v grep | awk '{print \$1}' | xargs kill -9"
-export PATH="$(systemd-path user-binaries):$PATH"
