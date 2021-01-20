fc48952141c41872c2c9a0c9fca87aefef9a572c
Carlos Gajardo <cjgajard@gmail.com>
2021-01-20 03:10:18 -0300

    Update gnome setup

--- gnome/setup.sh
+++ gnome/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down \
   '["<Shift><Super>j"]'
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up \
@@ -9,5 +8,5 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down \
   '["<Super>j"]'
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up \
   '["<Super>k"]'
-gsettings set org.gnome.nautilus.desktop trash-icon-visible false
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
