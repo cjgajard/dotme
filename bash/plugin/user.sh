4247e289221145941db168f2ad6d8056509056f2
Carlos Gajardo <cjgajard@gmail.com>
2021-02-02 23:42:45 -0300

    Add srcenv to user/plugin

--- bash/plugin/user.sh
+++ bash/plugin/user.sh
@@ -6,6 +6,11 @@ fnkill () {
   whoami | xargs ps -o pid,cmd,args -u | grep "$@" | grep -v grep \
     | awk '{print $1}' | xargs kill -9
 }
+srcenv() {
+  set -o allexport
+  . ${1:-.env}
+  set +o allexport
+}
 alias prettyjson='python3 -m json.tool'
 alias tree="tree -a -I '.git|node_modules|.env|__pycache__'"
 alias echopath='echo $PATH | tr : "\n"'
