81fafb8f7fdb755c9b02f84a1ad9a84a6f7801fa
Carlos Gajardo <cjgajard@gmail.com>
2021-02-20 17:50:27 -0300

    Update init.el

--- nvim/emacs.d/init.el
+++ nvim/emacs.d/init.el
@@ -1,19 +1,25 @@
+(setq inhibit-startup-echo-area-message "USERNAME")
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message "")
+(setq make-backup-files nil)
+(setq-default frame-title-format "%f")
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+
+(setq evil-disable-insert-state-bindings t)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-
-(add-to-list 'default-frame-alist '(height . 24))
-(add-to-list 'default-frame-alist '(width . 80))
-(setq inhibit-startup-echo-area-message "USERNAME")
-(setq initial-scratch-message "")
-(setq-default frame-title-format "%f")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dracula-theme evil)))
+ '(package-selected-packages '(go-mode dracula-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,10 +28,4 @@
  )
 (require 'evil)
 (evil-mode 1)
-
-(setq inhibit-startup-screen t)
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-
 (load-theme 'dracula t)
