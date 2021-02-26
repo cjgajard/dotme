e2c7e0bc2fdf74d11ab81a930d5b77f994b34fb4
Carlos Gajardo <cjgajard@gmail.com>
2021-02-26 10:22:28 -0300

    Move psfind and pskill to its own lib

--- bash/plugin/user.sh
+++ bash/plugin/user.sh
@@ -1,13 +1,3 @@
-psfind() {
-  if [ -z "$1" ]; then
-    echo "Usage: ${FUNCNAME[0]} [grep-options] SEARCH_TEXT"
-    return 1
-  fi
-  whoami | xargs ps -o pid,args -u | grep "$@" | grep -v grep
-}
-pskill() {
-  psfind "$@" | awk '{print $1}' | xargs kill -9
-}
 srcenv() {
   set -o allexport
   . ${1:-.env}
