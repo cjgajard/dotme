6552da7d6c693d752d012baa2fa15129fdd40701
Carlos Gajardo <cjgajard@gmail.com>
2021-02-26 10:20:33 -0300

    Fix pskill not using its arguments

--- bash/plugin/user.sh
+++ bash/plugin/user.sh
@@ -1,12 +1,12 @@
-psfind () {
+psfind() {
   if [ -z "$1" ]; then
     echo "Usage: ${FUNCNAME[0]} [grep-options] SEARCH_TEXT"
     return 1
   fi
   whoami | xargs ps -o pid,args -u | grep "$@" | grep -v grep
 }
-pskill () {
-  psfind | awk '{print $1}' | xargs kill -9
+pskill() {
+  psfind "$@" | awk '{print $1}' | xargs kill -9
 }
 srcenv() {
   set -o allexport
