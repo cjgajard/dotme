af8a17ea166314cc98308d681d9f9aa4be3e349c
Carlos Gajardo <cjgajard@gmail.com>
2022-02-27 21:44:37 -0300

    Add vtrgb generator for gnome theme saver

--- gnome/save-theme.sh
+++ gnome/save-theme.sh
@@ -8,6 +8,7 @@ fi
 
 if [[ -z "$PROFILE_ID" ]]; then
   echo missing \$PROFILE_ID or \$1
+  exit
 fi
 
 DIR=/org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}
@@ -22,11 +23,24 @@ if command -v dconf >/dev/null && [[ -n "$PROFILE_ID" ]]; then
 END
 
 dconf list $DIR/ | while read line; do
-cat <<-END
+    cat <<-END
     dconf write \$DIR/$line "$(dconf read $DIR/$line)"
 END
 done
 
-cat <<-'END'
+cat <<-END
 fi
+
+: <<-VTRGB
+END
+
+ruby <<-END
+re = /\((\d{1,3}),(\d{1,3}),(\d{1,3})\)/
+src = \`dconf read $DIR/palette\`.scan re
+src[0] = \`dconf read $DIR/background-color\`.match(re).captures
+src.transpose.each { |x| puts x.join "," }
+END
+
+cat <<-END
+VTRGB
 END
