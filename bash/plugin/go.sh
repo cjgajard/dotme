7f7b6a4ac94db83d46d30001f902ca67f9badd97
Carlos Gajardo <cjgajard@gmail.com>
2022-09-02 08:29:36 -0400

    Add go and gocomplete to PATH in bash plugin go
    Try adding /usr/local/go to PATH if go is not present

--- bash/plugin/go.sh
+++ bash/plugin/go.sh
@@ -1,5 +1,15 @@
+if ! command -v go &>/dev/null; then
+  export PATH="$PATH:/usr/local/go/bin"
+fi
+
 if command -v go &>/dev/null; then
   export PATH="$(go env GOPATH)/bin:$PATH"
+
+  gocompletepath=`type -p gocomplete`
+  if [[ -x "$gocompletepath" ]]; then
+    complete -C "$gocompletepath" go
+  fi
+  unset $gocompletepath
 fi
 
 go_ctags() {
