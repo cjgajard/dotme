aabb289ddba930607317c2d35b48c7540b16c3e0
Carlos Gajardo <cjgajard@gmail.com>
2023-02-10 02:47:24 -0300

    bash plugin ripley: add checkout-tools to path if present

--- bash/plugin/ripley.sh
+++ bash/plugin/ripley.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-export PATH="$HOME/ripley/checkout-tools/bin:$PATH"
-
-# if type -p srcenv; then
-#   srcenv ~/ripley/checkout-tools/.env
-#   srcenv
-# fi
+if [[ -e $HOME/ripleyx/checkout-tools/bin ]]; then
+    export PATH="$HOME/ripleyx/checkout-tools/bin:$PATH"
+fi
