75bd08d40a42a4dfdfc2fa40b4694f2dc4ae99db
Carlos Gajardo <cjgajard@gmail.com>
2023-02-10 02:46:28 -0300

    Move bash completion to its own file

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,3 +1,21 @@
+#!/usr/bin/env bash
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+if [ -f /usr/share/bash-completion/completions/git ]; then
+  . /usr/share/bash-completion/completions/git
+fi
+
+if [ -f /usr/share/bash-completion/completions/tig ]; then
+  . /usr/share/bash-completion/completions/tig
+fi
+
 _me_load() {
   local type="${1#me_}"
   local bash="$(cd `dirname ${BASH_SOURCE}`; pwd -P)"
